8d60518e47ab653541a241c8765bd98d1d188f23
JulieAnn Scherer <schererjulie96@gmail.com>
2022-10-27 14:35:42 -0500

    href value changed to /

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import logo from './ct-logo.png';
 
 export default function Logo() {
     return (
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
             <div className='d-flex align-items-center '>
                 <img src={logo} alt="" className="App-logo d-inline-block align-text-top" />
                 <h1 className="mx-2 text-white display-6">Coding Temple</h1>
